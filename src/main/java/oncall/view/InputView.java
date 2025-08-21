adb4db7a22808eddd9a42bd83351a22ac3e34833
kangrae <kangrae0501@gmail.com>
2025-08-21 17:24:27 +0900

    feat: 시작 월과 요일을 입력받는 메서드 구현
    - month와 week를 String 배열로 반환한다.

---
+++ src/main/java/oncall/view/InputView.java
@@ -0,0 +1,13 @@
+package oncall.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    public static String[] readStartMonthAndWeek() {
+        String month = Console.readLine();
+        String week = Console.readLine();
+        return new String[]{month, week};
+    }
+
+}
