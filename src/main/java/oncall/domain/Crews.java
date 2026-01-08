816d0ab8b02d4e6cbd87f77063f9dc4e5a6d90bc
kangrae <kangrae0501@gmail.com>
2026-01-08 22:42:28 +0900

    feat: 휴일, 평일에 한명씩 들어가도록 검증

--- src/main/java/oncall/domain/Crews.java
+++ src/main/java/oncall/domain/Crews.java
@@ -3,8 +3,10 @@ package oncall.domain;
 import static oncall.config.AppConfig.ERROR_MSG;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Crews {
 
@@ -13,6 +15,7 @@ public class Crews {
 
     public Crews(Map<Type, List<Crew>> crews) {
         validateCrewsSize(crews);
+        validateIsSameCrews(crews);
         this.crews = crews;
         this.index = new HashMap<>();
         for (Type type : Type.values()) {
@@ -29,6 +32,25 @@ public class Crews {
 
     private void validateCrewsSize(Map<Type, List<Crew>> crews) {
         if (crews.get(Type.WEEKDAY).size() > 35) {
+            throw new IllegalArgumentException(ERROR_MSG);
+        }
+    }
+
+    private void validateIsSameCrews(Map<Type, List<Crew>> crews) {
+        int size = crews.get(Type.WEEKDAY).size();
+        for (Type type : Type.values()) {
+            if (crews.get(type).size() != size) {
+                throw new IllegalArgumentException(ERROR_MSG);
+            }
+        }
+
+        Set<String> weekday = new HashSet<>(crews.get(Type.WEEKDAY).stream()
+                .map(Crew::getName)
+                .toList());
+        Set<String> weekend = new HashSet<>(crews.get(Type.WEEKEND).stream()
+                .map(Crew::getName)
+                .toList());
+        if (weekday.equals(weekend)) {
             throw new IllegalArgumentException(ERROR_MSG);
         }
     }
