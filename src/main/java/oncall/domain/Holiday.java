3bb1a8e2c9fc8a3a192636acf9538821b71327e3
kangrae <kangrae0501@gmail.com>
2026-01-08 21:49:17 +0900

    feat: 공휴일 enum 구현

---
+++ src/main/java/oncall/domain/Holiday.java
@@ -0,0 +1,22 @@
+package oncall.domain;
+
+import java.time.MonthDay;
+
+public enum Holiday {
+
+    DAY0101(MonthDay.of(1, 1)),
+    DAY0301(MonthDay.of(3, 1)),
+    DAY0505(MonthDay.of(5, 5)),
+    DAY0606(MonthDay.of(6, 6)),
+    DAY0815(MonthDay.of(8, 15)),
+    DAY1003(MonthDay.of(10, 3)),
+    DAY1009(MonthDay.of(10, 9)),
+    DAY1225(MonthDay.of(12, 25));
+
+    private final MonthDay monthDay;
+
+    Holiday(MonthDay monthDay) {
+        this.monthDay = monthDay;
+    }
+
+}
